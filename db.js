3223aece0ce4ad47a3c4af0fcf2c2bf9d0c4c49a
Danben Erick <danbencb08@outlook.es>
2024-09-06 21:06:24 -0500

    DB

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-    host: '172.16.10.100',
+    host: '172.233.176.171',
     user: 'cepre',
     password: 'cepre-asistencia-pedro-castillo',
     database: 'cepre_asistencia',   
